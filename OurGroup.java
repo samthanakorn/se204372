8eacd7027af67a7646f609750a2521a7817bd250
CHUSANA <chusana.w@ku.th>
2022-01-24 23:08:49 +0700

    Update OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -6,7 +6,7 @@ public class OurGroup {
     public OurGroup()
     {
         groupMembers = new Vector<String>();
-        groupMembers.add("YOUR NAME"); //PUT YOUR NAME HERE
+        groupMembers.add("Chusana Wilaivanichwong"); //PUT YOUR NAME HERE
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
